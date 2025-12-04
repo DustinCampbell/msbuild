396480ef850f9b4aa12aa0a031fb6da3b3612d8f
Dustin Campbell <dustin@teamcampbell.org>
2025-12-04 10:22:32 -0800

    Allow args to be passed to control Ngen, inlining and ETW profiler
    Update MSBuild.Benchmarks to allow arguments to be passed to use the ETW profiler, disable NGEN/ReadyToRun images, and disable JIT inlining.

--- src/MSBuild.Benchmarks/Program.cs
+++ src/MSBuild.Benchmarks/Program.cs
@@ -1,20 +1,77 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using MSBuild.Benchmarks;
 
-namespace MSBuild.Benchmarks;
+var argList = new List<string>(args);
 
-internal static class Program
+ParseAndRemoveBooleanParameter(argList, "--etw", out var useEtwProfiler);
+ParseAndRemoveBooleanParameter(argList, "--disable-ngen", out var disableNgen);
+ParseAndRemoveBooleanParameter(argList, "--disable-inlining", out var disableJitInlining);
+
+return BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run(args, GetConfig(useEtwProfiler, disableNgen, disableJitInlining))
+    .ToExitCode();
+
+IConfig GetConfig(bool useEtwProfiler, bool disableNgen, bool disableJitInlining)
 {
-    private static int Main(string[] args)
-        => BenchmarkSwitcher
-            .FromAssembly(typeof(Program).Assembly)
-            .Run(args)
-            .ToExitCode();
+    if (Debugger.IsAttached)
+    {
+        return new DebugInProcessConfig();
+    }
+
+    var config = DefaultConfig.Instance;
+
+    if (useEtwProfiler)
+    {
+        config = config.AddDiagnoser(new EtwProfiler());
+    }
+
+    var updateEnvironmentVars = disableNgen || disableJitInlining;
+
+    if (updateEnvironmentVars)
+    {
+        var job = Job.Default;
+
+        if (disableNgen)
+        {
+            job = job
+                .WithEnvironmentVariable("COMPlus_ZapDisable", "1")
+                .WithEnvironmentVariable("COMPlus_ReadyToRun", "0")
+                .WithEnvironmentVariable("DOTNET_ReadyToRun", "0");
+        }
+
+        if (disableJitInlining)
+        {
+            job = job
+                .WithEnvironmentVariable("COMPlus_JitNoInline", "1")
+                .WithEnvironmentVariable("DOTNET_JitNoInline", "0");
+        }
+
+        config = config.AddJob(job);
+    }
+
+    return config;
+}
+
+void ParseAndRemoveBooleanParameter(List<string> argsList, string parameter, out bool parameterValue)
+{
+    var parameterIndex = argsList.IndexOf(parameter);
+
+    if (parameterIndex != -1)
+    {
+        argsList.RemoveAt(parameterIndex);
+
+        parameterValue = true;
+    }
+    else
+    {
+        parameterValue = false;
+    }
 }
