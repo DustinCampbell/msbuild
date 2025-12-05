747c748182dc9e42d91ca7e3f9e19c59cc1af6d5
Dustin Campbell <dustin@teamcampbell.org>
2025-12-05 13:22:21 -0800

    Add more RefStack<T> methods
    - Add TryPop(out bool)
    - Add Pook()
    - Add PeekOrDefault()
    - Add PeekOrDefault(T)
    - Add MethodImpOptions.AggressiveInlining to public methods
    - unit tests

--- src/Build/Collections/RefStack.cs
+++ src/Build/Collections/RefStack.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Microsoft.Build.Shared;
 
 namespace Microsoft.Build.Collections;
 
@@ -22,7 +24,7 @@ internal ref struct RefStack<T>
     public readonly bool IsEmpty => _builder.Count == 0;
 
     /// <summary>
-    ///  Initializes a new instance of the <see cref="RefStack{T}"/> with the specified initial capacity.
+    ///  Initializes a new instance of the <see cref="RefStack{T}"/> struct with the specified initial capacity.
     /// </summary>
     /// <param name="initialCapacity">The initial capacity of the stack.</param>
     public RefStack(int initialCapacity)
@@ -31,9 +33,12 @@ internal ref struct RefStack<T>
     }
 
     /// <summary>
-    ///  Initializes a new instance of the <see cref="RefStack{T}"/> with the specified scratch buffer.
+    /// Initializes a new instance of the <see cref="RefStack{T}"/> struct with the specified scratch buffer.
     /// </summary>
     /// <param name="scratchBuffer">The initial buffer to use for storing elements.</param>
+    /// <remarks>
+    ///  <paramref name="scratchBuffer"/> should generally be stack-allocated to avoid heap allocations.
+    /// </remarks>
     public RefStack(Span<T> scratchBuffer)
     {
         _builder = new RefArrayBuilder<T>(scratchBuffer);
@@ -43,6 +48,7 @@ internal ref struct RefStack<T>
     ///  Releases the pooled array back to the shared <see cref="ArrayPool{T}"/>.
     ///  This method can be called multiple times safely.
     /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Dispose()
     {
         _builder.Dispose();
@@ -52,7 +58,8 @@ internal ref struct RefStack<T>
     ///  Pushes an item onto the top of the stack. The stack will automatically grow if needed.
     /// </summary>
     /// <param name="item">The item to push onto the stack.</param>
-    public void Push(in T item)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Push(T item)
     {
         _builder.Add(item);
     }
@@ -61,6 +68,7 @@ internal ref struct RefStack<T>
     ///  Removes and returns the item at the top of the stack.
     /// </summary>
     /// <returns>The item that was removed from the top of the stack.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public T Pop()
     {
         int lastIndex = _builder.Count - 1;
@@ -71,18 +79,84 @@ internal ref struct RefStack<T>
     }
 
     /// <summary>
+    ///  Attempts to remove and return the item at the top of the stack.
+    /// </summary>
+    /// <remarks>
+    ///  Use this method to safely attempt to pop an item from the stack without throwing an exception
+    ///  if the stack is empty.
+    /// </remarks>
+    /// <param name="item">
+    ///  When this method returns, contains the object removed from the top of the stack if the operation succeeds;
+    ///  otherwise, the default value for the type <typeparamref name="T"/>.
+    /// </param>
+    /// <returns>
+    ///  <see langword="true"/> if an object was successfully removed and returned from the stack; otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool TryPop([MaybeNullWhen(false)] out T item)
+    {
+        if (!IsEmpty)
+        {
+            item = Pop();
+            return true;
+        }
+
+        item = default;
+        return false;
+    }
+
+    /// <summary>
+    ///  Returns the item at the top of the stack without removing it.
+    /// </summary>
+    /// <returns>The item at the top of the stack.</returns>
+    /// <exception cref="InvalidOperationException">The stack is empty.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly T Peek()
+    {
+        if (IsEmpty)
+        {
+            ErrorUtilities.ThrowInvalidOperation("RefStack_Empty");
+        }
+
+        return _builder.AsSpan()[^1];
+    }
+
+    /// <summary>
+    ///  Returns the item at the top of the stack without removing it, or a default value if the stack is empty.
+    /// </summary>
+    /// <returns>
+    ///  The item at the top of the stack, or <see langword="default"/>(<typeparamref name="T"/>) if the stack is empty.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly T? PeekOrDefault()
+        => !IsEmpty ? _builder.AsSpan()[^1] : default;
+
+    /// <summary>
+    ///  Returns the item at the top of the stack without removing it, or a specified default value if the stack is empty.
+    /// </summary>
+    /// <param name="defaultValue">The default value to return if the stack is empty.</param>
+    /// <returns>
+    ///  The item at the top of the stack, or <paramref name="defaultValue"/> if the stack is empty.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public readonly T PeekOrDefault(T defaultValue)
+        => !IsEmpty ? _builder.AsSpan()[^1] : defaultValue;
+
+    /// <summary>
     ///  Attempts to peek at the item at the top of the stack without removing it.
     /// </summary>
     /// <param name="item">
     ///  When this method returns, contains the item at the top of the stack if the
     ///  stack is not empty; otherwise, the default value for the type.
     /// </param>
-    /// <returns><see langword="true"/> if the stack is not empty; otherwise, <see langword="false"/>.</returns>
+    /// <returns>
+    ///  <see langword="true"/> if the stack is not empty; otherwise, <see langword="false"/>.
+    /// </returns>
     public readonly bool TryPeek([MaybeNullWhen(false)] out T item)
     {
-        if (_builder.Count > 0)
+        if (!IsEmpty)
         {
-            item = _builder[_builder.Count - 1];
+            item = _builder.AsSpan()[^1];
             return true;
         }
 
