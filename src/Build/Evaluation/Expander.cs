967e594b97eff98475601d4ea4aab5b5c47affd6
Dustin Campbell <dustin@teamcampbell.org>
2025-12-10 12:35:15 -0800

    IntrinsicFunctionOverload improvments
    - Move to separate file
    - Use FrozenSet<string> for lookup rather than looping through an array.
    - General clean up

--- src/Build/Evaluation/Expander.cs
+++ src/Build/Evaluation/Expander.cs
@@ -4819,38 +4819,4 @@ namespace Microsoft.Build.Evaluation
             }
         }
     }
-
-#nullable enable
-
-    internal static class IntrinsicFunctionOverload
-    {
-        private static readonly string[] s_knownOverloadName = { "Add", "Subtract", "Multiply", "Divide", "Modulo", };
-
-        // Order by the TypeCode of the first parameter.
-        // When change wave is enabled, order long before double.
-        // Otherwise preserve prior behavior of double before long.
-        // For reuse, the comparer is cached in a non-generic type.
-        // Both comparer instances can be cached to support change wave testing.
-        private static IComparer<MemberInfo>? s_comparerLongBeforeDouble;
-
-        internal static IComparer<MemberInfo> IntrinsicFunctionOverloadMethodComparer => LongBeforeDoubleComparer;
-
-        private static IComparer<MemberInfo> LongBeforeDoubleComparer => s_comparerLongBeforeDouble ??= Comparer<MemberInfo>.Create((key0, key1) => SelectTypeOfFirstParameter(key0).CompareTo(SelectTypeOfFirstParameter(key1)));
-
-        internal static bool IsKnownOverloadMethodName(string methodName) => s_knownOverloadName.Any(name => string.Equals(name, methodName, StringComparison.OrdinalIgnoreCase));
-
-        private static TypeCode SelectTypeOfFirstParameter(MemberInfo member)
-        {
-            MethodBase? method = member as MethodBase;
-            if (method == null)
-            {
-                return TypeCode.Empty;
-            }
-
-            ParameterInfo[] parameters = method.GetParameters();
-            return parameters.Length > 0
-                ? Type.GetTypeCode(parameters[0].ParameterType)
-                : TypeCode.Empty;
-        }
-    }
 }
---
+++ src/Build/Evaluation/IntrinsicFunctionOverload.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Microsoft.Build.Evaluation;
+
+internal static class IntrinsicFunctionOverload
+{
+    private static readonly FrozenSet<string> s_knownOverloadNames = FrozenSet.Create(
+        StringComparer.OrdinalIgnoreCase, "Add", "Subtract", "Multiply", "Divide", "Modulo");
+
+    internal static IComparer<MemberInfo> IntrinsicFunctionOverloadMethodComparer
+        => LongBeforeDoubleComparer;
+
+    // Order by the TypeCode of the first parameter.
+    // When change wave is enabled, order long before double.
+    // Otherwise preserve prior behavior of double before long.
+    // For reuse, the comparer is cached in a non-generic type.
+    // Both comparer instances can be cached to support change wave testing.
+    private static IComparer<MemberInfo> LongBeforeDoubleComparer
+        => field ??= Comparer<MemberInfo>.Create((member1, member2) =>
+        {
+            return SelectTypeOfFirstParameter(member1).CompareTo(SelectTypeOfFirstParameter(member2));
+        });
+
+    internal static bool IsKnownOverloadMethodName(string methodName)
+        => s_knownOverloadNames.Contains(methodName);
+
+    private static TypeCode SelectTypeOfFirstParameter(MemberInfo member)
+    {
+        if (member is not MethodBase method)
+        {
+            return TypeCode.Empty;
+        }
+
+        return method.GetParameters() is [var parameter, ..]
+            ? Type.GetTypeCode(parameter.ParameterType)
+            : TypeCode.Empty;
+    }
+}
