a93b237d234b4b4b61dede9d211ec1cc1b941e7e
Dustin Campbell <dustin@teamcampbell.org>
2025-12-16 11:13:42 -0800

    Add KVP.Deconstruct polyfill
    Add polyfill extension method for KVP.Deconstruct on non-.NET.

---
+++ src/Framework/Polyfills/KeyValuePairExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Collections.Generic;
+
+internal static class KeyValuePairExtensions
+{
+#if !NET
+    public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value)
+    {
+        key = kvp.Key;
+        value = kvp.Value;
+    }
+#endif
+}
