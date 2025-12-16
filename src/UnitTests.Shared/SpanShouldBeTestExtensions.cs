9db4c652c38390f3d8e30978ff4c370cc3e14166
Dustin Campbell <dustin@teamcampbell.org>
2025-12-16 14:24:12 -0800

    Implement ExpressionParser.GetReferenceItemExpressions
    Port GetReferencedItemExpressions implementation from ExpressionShredder to ExpressionParser and remove substring allocations.

---
+++ src/UnitTests.Shared/SpanShouldBeTestExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Shouldly;
+
+[DebuggerStepThrough]
+[ShouldlyMethods]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class SpanShouldBeTestExtensions
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ShouldBe(
+        this ReadOnlySpan<char> actual,
+        ReadOnlySpan<char> expected,
+        string? customMessage = null)
+        => ShouldBe(actual, expected, customMessage, 0);
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ShouldBe(
+        this ReadOnlySpan<char> actual,
+        ReadOnlySpan<char> expected,
+        StringCompareShould options)
+        => ShouldBe(actual, expected, customMessage: null, options);
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ShouldBe(
+        this ReadOnlySpan<char> actual,
+        ReadOnlySpan<char> expected,
+        string? customMessage,
+        StringCompareShould options)
+        => actual.ToString().ShouldBe(expected.ToString(), customMessage, options);
+}
