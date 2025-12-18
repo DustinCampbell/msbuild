233fa6028f355723eff0c90b6fc940e1c83d8688
Dustin Campbell <dustin@teamcampbell.org>
2025-12-17 16:39:45 -0800

    Rewrite ExpressionParser.ReferencedItemExpressionsEnumerator
    Use TryParse* functions and remove Sink* functions

--- src/Build/Evaluation/ExpressionParser.ReferencedItemExpressionsEnumerator.cs
+++ src/Build/Evaluation/ExpressionParser.ReferencedItemExpressionsEnumerator.cs
@@ -1,170 +1,159 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using Microsoft.Build.Collections;
+
+#if !NET
+using Microsoft.Build.Utilities;
+#endif
 
 namespace Microsoft.Build.Evaluation;
 
 internal static partial class ExpressionParser
 {
-    internal ref struct ReferencedItemExpressionsEnumerator
+    internal ref struct ReferencedItemExpressionsEnumerator(ReadOnlyMemory<char> expression)
     {
-        private readonly string _expression;
-        private readonly int _end;
-        private int _position;
+        private readonly ReadOnlyMemory<char> _expression = expression;
 
-        public ReferencedItemExpressionsEnumerator(string expression, int start, int end)
-        {
-            _expression = expression;
-            _end = end;
+        private ReadOnlyMemory<char> _worker = expression;
+        private int _start;
+        private ItemExpressionCapture _current;
 
-            _position = expression.IndexOf('@', start, end - start);
-            if (_position < 0)
-            {
-                _position = int.MaxValue;
-            }
-        }
-
-        public ItemExpressionCapture Current { get; private set; }
+        public readonly ItemExpressionCapture Current => _current;
 
         public bool MoveNext()
         {
-            for (; _position < _end; _position++)
+            ReadOnlyMemory<char> memory = _worker;
+            int end = _start + _worker.Length;
+
+            while (!memory.IsEmpty)
             {
-                if (!Sink(_expression, ref _position, _end, '@', '('))
+                int start = end - memory.Length;
+
+                if (TryParseReferencedItemExpression(ref memory, start, out var capture))
                 {
-                    continue;
+                    _current = capture;
+                    _worker = memory;
+                    _start = _expression.Length - _worker.Length;
+                    return true;
                 }
-
-                // Start of a possible item list expression
-
-                // Store the index to backtrack to if this doesn't turn out to be a well
-                // formed expression. (Subtract one for the increment when we loop around.)
-                int restartPoint = _position - 1;
-
-                // Store the expression's start point
-                int startPoint = _position - 2;
-
-                SinkWhitespace(_expression, ref _position);
-
-                int startOfName = _position;
-
-                if (!SinkValidName(_expression, ref _position, _end))
+                else
                 {
-                    _position = restartPoint;
-                    continue;
+                    memory = memory[1..];
                 }
-
-                // '-' is a legitimate char in an item name, but we should match '->' as an arrow
-                // in '@(foo->'x')' rather than as the last char of the item name.
-                // The old regex accomplished this by being "greedy"
-                if (_end > _position && _expression[_position - 1] == '-' && _expression[_position] == '>')
-                {
-                    _position--;
-                }
-
-                // Grab the name, but continue to verify it's a well-formed expression
-                // before we store it.
-                var itemName = TextToken.FromBounds(_expression, startOfName, _position);
-
-                SinkWhitespace(_expression, ref _position);
-
-                bool transformOrFunctionFound = true;
-                using RefArrayBuilder<ItemExpressionCapture> transformExpressions = new(initialCapacity: 4);
-
-                // If there's an '->' eat it and the subsequent quoted expression or transform function
-                while (Sink(_expression, ref _position, _end, '-', '>') && transformOrFunctionFound)
-                {
-                    SinkWhitespace(_expression, ref _position);
-                    int startTransform = _position;
-
-                    bool isQuotedTransform = SinkSingleQuotedExpression(_expression, ref _position, _end);
-                    if (isQuotedTransform)
-                    {
-                        int startQuoted = startTransform + 1;
-                        int endQuoted = _position - 1;
-
-                        transformExpressions.Add(new(TextToken.FromBounds(_expression, startQuoted, endQuoted)));
-
-                        SinkWhitespace(_expression, ref _position);
-                        continue;
-                    }
-
-                    startTransform = _position;
-                    ItemExpressionCapture? functionCapture = SinkItemFunctionExpression(_expression, startTransform, ref _position, _end);
-                    if (functionCapture is ItemExpressionCapture functionCaptureValue)
-                    {
-                        transformExpressions.Add(functionCaptureValue);
-
-                        SinkWhitespace(_expression, ref _position);
-                        continue;
-                    }
-
-                    if (!isQuotedTransform && functionCapture == null)
-                    {
-                        _position = restartPoint;
-                        transformOrFunctionFound = false;
-                    }
-                }
-
-                if (!transformOrFunctionFound)
-                {
-                    continue;
-                }
-
-                SinkWhitespace(_expression, ref _position);
-
-                TextToken separator = TextToken.Missing;
-
-                // If there's a ',', eat it and the subsequent quoted expression
-                if (Sink(_expression, ref _position, ','))
-                {
-                    SinkWhitespace(_expression, ref _position);
-
-                    if (!Sink(_expression, ref _position, '\''))
-                    {
-                        _position = restartPoint;
-                        continue;
-                    }
-
-                    int closingQuote = _expression.IndexOf('\'', _position);
-                    if (closingQuote == -1)
-                    {
-                        _position = restartPoint;
-                        continue;
-                    }
-
-                    // separatorStart = _position - startPoint;
-                    separator = TextToken.FromBounds(_expression, _position, closingQuote);
-
-                    _position = closingQuote + 1;
-                }
-
-                SinkWhitespace(_expression, ref _position);
-
-                if (!Sink(_expression, ref _position, ')'))
-                {
-                    _position = restartPoint;
-                    continue;
-                }
-
-                int endPoint = _position;
-                _position--;
-
-                // Create an expression capture that encompasses the entire expression between the @( and the )
-                // with the item name and any separator contained within it
-                // and each transform expression contained within it (i.e. each ->XYZ)
-                ItemExpressionCapture expressionCapture = new(TextToken.FromBounds(_expression, startPoint, endPoint), itemName, separator, transformExpressions.ToImmutable());
-
-                Current = expressionCapture;
-                ++_position;
-
-                return true;
             }
 
-            Current = default;
-
+            _current = default;
             return false;
+        }
+
+        private static bool TryParseReferencedItemExpression(ref ReadOnlyMemory<char> memory, int start, out ItemExpressionCapture result)
+        {
+            result = default;
+
+            if (!memory.Span.StartsWith("@("))
+            {
+                return false;
+            }
+
+            int end = start + memory.Length;
+
+            var current = memory[2..].TrimStart();
+            int nameStart = end - current.Length;
+
+            if (!TryParseValidName(ref current, out var name))
+            {
+                return false;
+            }
+
+            // Grab the name, but continue to verify it's a well-formed expression
+            // before we store it.
+            TextToken itemName = new(name, nameStart);
+
+            current = current.TrimStart();
+
+            bool transformOrFunctionFound = true;
+            using RefArrayBuilder<ItemExpressionCapture> transformExpressions = new(initialCapacity: 4);
+
+            // If there's an '->' eat it and the subsequent quoted expression or transform function
+            while (transformOrFunctionFound && current.Span.StartsWith("->"))
+            {
+                current = current[2..].TrimStart();
+
+                int transformStart = end - current.Length;
+
+                if (TryParseSingleQuotedExpression(ref current, out var quotedExpression))
+                {
+                    // Remove quotes.
+                    quotedExpression = quotedExpression[1..^1];
+                    transformStart++;
+
+                    transformExpressions.Add(new(new(quotedExpression, transformStart)));
+
+                    current = current.TrimStart();
+                    continue;
+                }
+
+                if (TryParseItemExpressionCapture(ref current, transformStart, out var capture))
+                {
+                    transformExpressions.Add(capture);
+
+                    current = current.TrimStart();
+                    continue;
+                }
+
+                transformOrFunctionFound = false;
+            }
+
+            if (!transformOrFunctionFound)
+            {
+                return false;
+            }
+
+            current = current.TrimStart();
+
+            TextToken separator = TextToken.Missing;
+
+            // If there's a ',', eat it and the subsequent quoted expression
+            if (current.Span is [',', ..])
+            {
+                current = current[1..].TrimStart();
+
+                if (current.Span is not ['\'', ..])
+                {
+                    return false;
+                }
+
+                current = current[1..];
+
+                int closingQuote = current.Span.IndexOf('\'');
+                if (closingQuote == -1)
+                {
+                    return false;
+                }
+
+                int separatorStart = end - current.Length;
+                separator = new(current[..closingQuote], separatorStart);
+
+                current = current[(closingQuote + 1)..].TrimStart();
+            }
+
+            current = current.TrimStart();
+
+            if (current.Span is not [')', ..])
+            {
+                return false;
+            }
+
+            // Create an expression capture that encompasses the entire expression between the @( and the )
+            // with the item name and any separator contained within it
+            // and each transform expression contained within it (i.e. each ->XYZ)
+            int length = memory.Length - current.Length + 1;
+            result = new(new(memory[..length], start), itemName, separator, transformExpressions.ToImmutable());
+            memory = current[1..];
+
+            return true;
         }
     }
 }
