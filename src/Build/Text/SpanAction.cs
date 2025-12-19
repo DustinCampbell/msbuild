53ab693636b0ea490739de3d5959508616f848bd
Dustin Campbell <dustin@teamcampbell.org>
2025-12-19 15:57:01 -0800

    Copy StringSegment and friends from Touki

---
+++ src/Build/Text/SpanAction.cs
@@ -0,0 +1,8 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !NET
+namespace System.Buffers;
+
+public delegate void SpanAction<T, in TArg>(Span<T> span, TArg arg);
+#endif
