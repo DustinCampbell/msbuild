be95e18e9c257ed89a2e3109583bca032701e10f
Dustin Campbell <dustin@teamcampbell.org>
2025-12-19 15:54:08 -0800

    Add TypeForward for CallerArgumentExpressionAttribute polyfill

--- src/Framework/Polyfills/CallerArgumentExpressionAttribute.cs
+++ src/Framework/Polyfills/CallerArgumentExpressionAttribute.cs
@@ -1,17 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if NET
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Runtime.CompilerServices.CallerArgumentExpressionAttribute))]
+#else
+
 namespace System.Runtime.CompilerServices;
 
-#if !NET
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-internal sealed class CallerArgumentExpressionAttribute : Attribute
+internal sealed class CallerArgumentExpressionAttribute(string parameterName) : Attribute
 {
-    public CallerArgumentExpressionAttribute(string parameterName)
-    {
-        ParameterName = parameterName;
-    }
-
-    public string ParameterName { get; }
+    public string ParameterName { get; } = parameterName;
 }
 #endif
