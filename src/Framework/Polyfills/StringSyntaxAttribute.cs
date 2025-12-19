95ba1cfd2fbbb692cd9fe527d580b6dc62d0ed9a
Dustin Campbell <dustin@teamcampbell.org>
2025-12-19 15:47:07 -0800

    Move StringSyntaxAttribute polyfill to correct namespace
    StringSyntaxAttribute should be defined in the System.Diagnostics.CodeAnalysis namespace but the MSBuild polyfill is in System. This change puts it in the right namespace and adds a type forward when on .NET.
    
    In addition, Display the files from the Microsoft.CodeAnalysis.Contracts source package in a Contracts folder rather than the root of the project.

--- src/Framework/Polyfills/StringSyntaxAttribute.cs
+++ src/Framework/Polyfills/StringSyntaxAttribute.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if !NET
+#if NET
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Diagnostics.CodeAnalysis.StringSyntaxAttribute))]
+#else
 
-using System;
+namespace System.Diagnostics.CodeAnalysis;
 
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
 internal sealed class StringSyntaxAttribute : Attribute
