d99d51a55550c09e47dce9de5562a6df7b4433c8
Dustin Campbell <dustin@teamcampbell.org>
2025-12-18 16:44:00 -0800

    Clean up Evaluator_Tests
    - Clean up ImportLoadingScenarioTestData and use TheoryData.
    - Remove duplicate scenario from ImportLoadingScenarioTestData.
    - Use interpolated strings and raw string literals throughout.
    - Enable nullability and fix warnings.
    - Change Assert.True(ReferenceEquals(...)) to Assert.Same(...).
    - Use simple using statements.
    - General clean up.

---
+++ src/UnitTests.Shared/TheoryDataExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xunit;
+
+public static class TheoryDataExtensions
+{
+    public static void Add<T1, T2>(this TheoryData<T1, T2> data, (T1, T2) item)
+        => data.Add(item.Item1, item.Item2);
+
+    public static void Add<T1, T2, T3>(this TheoryData<T1, T2, T3> data, (T1, T2, T3) item)
+        => data.Add(item.Item1, item.Item2, item.Item3);
+
+    public static void Add<T1, T2, T3, T4>(this TheoryData<T1, T2, T3, T4> data, (T1, T2, T3, T4) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4);
+
+    public static void Add<T1, T2, T3, T4, T5>(this TheoryData<T1, T2, T3, T4, T5> data, (T1, T2, T3, T4, T5) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5);
+
+    public static void Add<T1, T2, T3, T4, T5, T6>(this TheoryData<T1, T2, T3, T4, T5, T6> data, (T1, T2, T3, T4, T5, T6) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5, item.Item6);
+
+    public static void Add<T1, T2, T3, T4, T5, T6, T7>(this TheoryData<T1, T2, T3, T4, T5, T6, T7> data, (T1, T2, T3, T4, T5, T6, T7) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5, item.Item6, item.Item7);
+
+    public static void Add<T1, T2, T3, T4, T5, T6, T7, T8>(this TheoryData<T1, T2, T3, T4, T5, T6, T7, T8> data, (T1, T2, T3, T4, T5, T6, T7, T8) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5, item.Item6, item.Item7, item.Item8);
+
+    public static void Add<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this TheoryData<T1, T2, T3, T4, T5, T6, T7, T8, T9> data, (T1, T2, T3, T4, T5, T6, T7, T8, T9) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5, item.Item6, item.Item7, item.Item8, item.Item9);
+
+    public static void Add<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this TheoryData<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> data, (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) item)
+        => data.Add(item.Item1, item.Item2, item.Item3, item.Item4, item.Item5, item.Item6, item.Item7, item.Item8, item.Item9, item.Item10);
+}
