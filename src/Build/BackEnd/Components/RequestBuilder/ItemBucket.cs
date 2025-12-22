8536e8b5aa7a4eab11d9353aa1d859e84958359e
Dustin Campbell <dustin@teamcampbell.org>
2025-12-22 08:29:29 -0800

    Clean up SringMetadataTable
    - Enable nullability and add annotations
    - Add static Empty field
    - Make constructor private
    - Add static Create methods

--- src/Build/BackEnd/Components/RequestBuilder/ItemBucket.cs
+++ src/Build/BackEnd/Components/RequestBuilder/ItemBucket.cs
@@ -103,7 +103,7 @@ namespace Microsoft.Build.BackEnd
         /// <param name="loggingContext"></param>
         internal void Initialize(LoggingContext loggingContext)
         {
-            _expander = new Expander<ProjectPropertyInstance, ProjectItemInstance>(_lookup, _lookup, new StringMetadataTable(_metadata), FileSystems.Default, loggingContext);
+            _expander = new Expander<ProjectPropertyInstance, ProjectItemInstance>(_lookup, _lookup, StringMetadataTable.Create(_metadata), FileSystems.Default, loggingContext);
         }
 
         #endregion
