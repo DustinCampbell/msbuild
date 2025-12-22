32bd0eaf732f85d13a6662e20b447051f0d26dd9
Dustin Campbell <dustin@teamcampbell.org>
2025-12-22 08:29:29 -0800

    Clean up SringMetadataTable
    - Enable nullability and add annotations
    - Add static Empty field
    - Make constructor private
    - Add static Create methods

--- src/Build/Evaluation/StringMetadataTable.cs
+++ src/Build/Evaluation/StringMetadataTable.cs
@@ -4,78 +4,88 @@
 using System;
 using System.Collections.Generic;
 
-#nullable disable
+namespace Microsoft.Build.Evaluation;
 
-namespace Microsoft.Build.Evaluation
+/// <summary>
+/// Wraps a table of metadata values in which keys
+/// may be qualified ("itemtype.name") or unqualified ("name").
+/// </summary>
+internal sealed class StringMetadataTable : IMetadataTable
 {
+    public static readonly StringMetadataTable Empty = new(null);
+
     /// <summary>
-    /// Wraps a table of metadata values in which keys
-    /// may be qualified ("itemtype.name") or unqualified ("name").
+    /// Table of metadata values.
+    /// Each key may be qualified ("itemtype.name") or unqualified ("name").
+    /// Unqualified are considered to apply to all item types.
+    /// May be null, if empty.
     /// </summary>
-    internal class StringMetadataTable : IMetadataTable
+    private readonly Dictionary<string, string>? _metadata;
+
+    /// <summary>
+    /// Constructor taking a table of metadata in which keys
+    /// may be a mixture of qualified ("itemtype.name") and unqualified ("name").
+    /// Unqualified keys are considered to apply to all item types.
+    /// Metadata may be null, indicating it is empty.
+    /// </summary>
+    private StringMetadataTable(Dictionary<string, string>? metadata)
     {
-        /// <summary>
-        /// Table of metadata values.
-        /// Each key may be qualified ("itemtype.name") or unqualified ("name").
-        /// Unqualified are considered to apply to all item types.
-        /// May be null, if empty.
-        /// </summary>
-        private Dictionary<string, string> _metadata;
+        _metadata = metadata;
+    }
 
-        /// <summary>
-        /// Constructor taking a table of metadata in which keys
-        /// may be a mixture of qualified ("itemtype.name") and unqualified ("name").
-        /// Unqualified keys are considered to apply to all item types.
-        /// Metadata may be null, indicating it is empty.
-        /// </summary>
-        internal StringMetadataTable(Dictionary<string, string> metadata)
+    public static StringMetadataTable Create(Dictionary<string, string>? metadata)
+        => metadata == null || metadata.Count == 0
+            ? Empty
+            : new StringMetadataTable(metadata);
+
+    public static StringMetadataTable Create(params ReadOnlySpan<(string Name, string Value)> metadata)
+    {
+        if (metadata.Length == 0)
         {
-            _metadata = metadata;
+            return Empty;
         }
 
-        /// <summary>
-        /// Retrieves any value we have in our metadata table for the metadata name specified.
-        /// If no value is available, returns empty string.
-        /// </summary>
-        public string GetEscapedValue(string name)
+        var dictionary = new Dictionary<string, string>(metadata.Length, StringComparer.OrdinalIgnoreCase);
+
+        foreach (var (name, value) in metadata)
         {
-            return GetEscapedValue(null, name);
+            dictionary.Add(name, value);
         }
 
-        /// <summary>
-        /// Retrieves any value we have in our metadata table for the metadata name and item type specified.
-        /// If no value is available, returns empty string.
-        /// </summary>
-        public string GetEscapedValue(string itemType, string name)
+        return new StringMetadataTable(dictionary);
+    }
+
+    /// <summary>
+    /// Retrieves any value we have in our metadata table for the metadata name specified.
+    /// If no value is available, returns empty string.
+    /// </summary>
+    public string GetEscapedValue(string name)
+        => GetEscapedValue(null, name);
+
+    /// <summary>
+    /// Retrieves any value we have in our metadata table for the metadata name and item type specified.
+    /// If no value is available, returns empty string.
+    /// </summary>
+    public string GetEscapedValue(string? itemType, string name)
+        => GetEscapedValueIfPresent(itemType, name) ?? string.Empty;
+
+    /// <summary>
+    /// Retrieves any value we have in our metadata table for the metadata name and item type specified.
+    /// If no value is available, returns null.
+    /// </summary>
+    public string? GetEscapedValueIfPresent(string? itemType, string name)
+    {
+        if (_metadata == null)
         {
-            return GetEscapedValueIfPresent(itemType, name) ?? String.Empty;
+            return null;
         }
 
-        /// <summary>
-        /// Retrieves any value we have in our metadata table for the metadata name and item type specified.
-        /// If no value is available, returns null.
-        /// </summary>
-        public string GetEscapedValueIfPresent(string itemType, string name)
-        {
-            if (_metadata == null)
-            {
-                return null;
-            }
+        string key = itemType == null
+            ? name
+            : $"{itemType}.{name}";
 
-            string key;
-            if (itemType == null)
-            {
-                key = name;
-            }
-            else
-            {
-                key = $"{itemType}.{name}";
-            }
+        _metadata.TryGetValue(key, out string? value);
 
-            string value;
-            _metadata.TryGetValue(key, out value);
-
-            return value;
-        }
+        return value;
     }
 }
