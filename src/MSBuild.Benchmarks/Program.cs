acaa4f5a3bc11b2684989641fbb1deb75b54ce36
Dustin Campbell <dustin@teamcampbell.org>
2026-01-08 08:47:56 -0800

    Add MSBuild.Benchmarks project
    Add a new project that will contain benchmarks using BenchmarkDotnet.

---
+++ src/MSBuild.Benchmarks/Program.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using MSBuild.Benchmarks;
+
+var argList = new List<string>(args);
+
+ParseAndRemoveBooleanParameter(argList, "--collect-etw", out bool collectEtw);
+ParseAndRemoveBooleanParameter(argList, "--disable-ngen", out bool disableNGen);
+ParseAndRemoveBooleanParameter(argList, "--disable-inlining", out bool disableJitInlining);
+
+return BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run([.. argList], GetConfig(collectEtw, disableNGen, disableJitInlining))
+    .ToExitCode();
+
+static IConfig GetConfig(bool collectEtw, bool disableNGen, bool disableJitInlining)
+{
+    if (Debugger.IsAttached)
+    {
+        return new DebugInProcessConfig();
+    }
+
+    IConfig config = DefaultConfig.Instance;
+    Job job = Job.Default;
+
+    if (collectEtw)
+    {
+        config = config.AddDiagnoser(new EtwProfiler());
+    }
+
+    if (disableNGen)
+    {
+        job = job
+            .WithEnvironmentVariable("COMPlus_ZapDisable", "1")
+            .WithEnvironmentVariable("COMPlus_ReadyToRun", "0")
+            .WithEnvironmentVariable("DOTNET_ReadyToRun", "0");
+    }
+
+    if (disableNGen)
+    {
+        job = job
+            .WithEnvironmentVariable("COMPlus_JitNoInline", "1")
+            .WithEnvironmentVariable("DOTNET_JitNoInline", "0");
+    }
+
+    config = config.AddJob(job);
+
+    return config;
+}
+
+static void ParseAndRemoveBooleanParameter(List<string> argsList, string parameter, out bool parameterValue)
+{
+    int parameterIndex = argsList.IndexOf(parameter);
+
+    if (parameterIndex != -1)
+    {
+        argsList.RemoveAt(parameterIndex);
+
+        parameterValue = true;
+    }
+    else
+    {
+        parameterValue = false;
+    }
+}
