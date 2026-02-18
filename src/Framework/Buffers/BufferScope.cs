74fde953161cbc3897e9ed5d0b24db088938f1b5
Dustin Campbell <dustin@teamcampbell.org>
2026-02-18 10:42:41 -0800

    Add BufferScope<T> and TypeInfo<T> helper types
    Add BufferScope<T> and TypeInfo<T> helper types along with unit tests.

---
+++ src/Framework/Buffers/BufferScope.cs
@@ -0,0 +1,199 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.Build.Buffers;
+
+/// <summary>
+///  Allows renting a buffer from <see cref="ArrayPool{T}"/> with a using statement. Can be used directly as if it
+///  were a <see cref="Span{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of elements in the buffer.</typeparam>
+internal ref struct BufferScope<T>
+{
+    private T[]? _array;
+    private Span<T> _span;
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="BufferScope{T}"/> struct with a minimum length.
+    /// </summary>
+    /// <param name="minimumLength">The required minimum length.</param>
+    public BufferScope(int minimumLength)
+    {
+        _array = ArrayPool<T>.Shared.Rent(minimumLength);
+        _span = _array;
+    }
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="BufferScope{T}"/> struct with an initial buffer.
+    ///  Useful for creating with an initial stack allocated buffer.
+    /// </summary>
+    /// <param name="initialBuffer">The initial buffer to use.</param>
+    public BufferScope(Span<T> initialBuffer)
+    {
+        _array = null;
+        _span = initialBuffer;
+    }
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="BufferScope{T}"/> struct with an initial buffer and a minimum length.
+    ///  Useful for creating with an initial stack allocated buffer.
+    /// </summary>
+    /// <param name="initialBuffer">The initial buffer to use.</param>
+    /// <param name="minimumLength">
+    ///  The required minimum length. If the <paramref name="initialBuffer"/> is not large enough, this will rent from
+    ///  the shared <see cref="ArrayPool{T}"/>.
+    /// </param>
+    /// <remarks>
+    ///  <example>
+    ///   Creating with a stack allocated buffer:
+    ///   <code>using BufferScope&lt;char> buffer = new(stackalloc char[64]);</code>
+    ///  </example>
+    /// </remarks>
+    public BufferScope(Span<T> initialBuffer, int minimumLength)
+    {
+        if (initialBuffer.Length >= minimumLength)
+        {
+            _array = null;
+            _span = initialBuffer;
+        }
+        else
+        {
+            _array = ArrayPool<T>.Shared.Rent(minimumLength);
+            _span = _array;
+        }
+    }
+
+    /// <summary>
+    ///  Releases the rented array back to the <see cref="ArrayPool{T}"/> if one was rented.
+    /// </summary>
+    public void Dispose()
+    {
+        // Clear the span to avoid accidental use after returning the array.
+        _span = default;
+
+        if (_array is T[] array)
+        {
+            ArrayPool<T>.Shared.Return(array, clearArray: TypeInfo<T>.IsReferenceOrContainsReferences());
+        }
+
+        _array = null;
+    }
+
+    /// <summary>
+    ///  Ensure that the buffer has enough space for <paramref name="capacity"/> number of elements.
+    /// </summary>
+    /// <param name="capacity">The minimum number of elements the buffer should be able to hold.</param>
+    /// <param name="copy"><see langword="true"/> to copy the existing elements when new space is allocated.</param>
+    /// <remarks>
+    ///  Consider if creating new <see cref="BufferScope{T}"/> instances is possible and cleaner than using
+    ///  this method.
+    /// </remarks>
+    public void EnsureCapacity(int capacity, bool copy = false)
+    {
+        if (_span.Length >= capacity)
+        {
+            return;
+        }
+
+        // Keep method separate for better inlining.
+        IncreaseCapacity(capacity, copy);
+    }
+
+    private void IncreaseCapacity(int capacity, bool copy)
+    {
+        Debug.Assert(capacity > _span.Length, "Capacity must be greater than the current span length.");
+
+        T[] newArray = ArrayPool<T>.Shared.Rent(capacity);
+        if (copy)
+        {
+            _span.CopyTo(newArray);
+        }
+
+        if (_array is { } array)
+        {
+            ArrayPool<T>.Shared.Return(array, clearArray: TypeInfo<T>.IsReferenceOrContainsReferences());
+        }
+
+        _array = newArray;
+        _span = _array;
+    }
+
+    /// <summary>
+    ///  Gets the number of elements in the buffer.
+    /// </summary>
+    public readonly int Length
+        => _span.Length;
+
+    /// <summary>
+    ///  Gets or sets the element at the specified index.
+    /// </summary>
+    /// <param name="i">The zero-based index of the element to get or set.</param>
+    /// <returns>
+    ///  The element at the specified index.
+    /// </returns>
+    public ref T this[int i]
+        => ref _span[i];
+
+    /// <summary>
+    ///  Returns the buffer as a <see cref="Span{T}"/>.
+    /// </summary>
+    /// <returns>
+    ///  A span that represents the buffer.
+    /// </returns>
+    public readonly Span<T> AsSpan()
+        => _span;
+
+    /// <summary>
+    ///  Returns an enumerator for the buffer's backing <see cref="Span{T}"/>.
+    /// </summary>
+    public readonly Span<T>.Enumerator GetEnumerator()
+        => _span.GetEnumerator();
+
+    /// <inheritdoc cref="Span{T}.GetPinnableReference"/>
+    /// <remarks>
+    ///  This is used by C# to enable using the buffer in a fixed statement.
+    /// </remarks>
+    public readonly ref T GetPinnableReference()
+        => ref _span.GetPinnableReference();
+
+    /// <summary>
+    ///  Forms a slice out of the buffer starting at a specified index for a specified length.
+    /// </summary>
+    /// <param name="start">The index at which to begin the slice.</param>
+    /// <param name="length">The desired length of the slice.</param>
+    /// <returns>
+    ///  A span that consists of <paramref name="length"/> elements from the buffer starting at <paramref name="start"/>.
+    /// </returns>
+    public readonly Span<T> Slice(int start, int length)
+        => _span.Slice(start, length);
+
+    /// <summary>
+    ///  Returns a string representation of the buffer.
+    /// </summary>
+    /// <returns>
+    ///  A string representation of the buffer.
+    /// </returns>
+    public override readonly string ToString()
+        => _span.ToString();
+
+    /// <summary>
+    ///  Implicitly converts a <see cref="BufferScope{T}"/> to a <see cref="Span{T}"/>.
+    /// </summary>
+    /// <param name="scope">The buffer scope to convert.</param>
+    /// <returns>A span that represents the buffer.</returns>
+    public static implicit operator Span<T>(BufferScope<T> scope)
+        => scope._span;
+
+    /// <summary>
+    ///  Implicitly converts a <see cref="BufferScope{T}"/> to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <param name="scope">The buffer scope to convert.</param>
+    /// <returns>A read-only span that represents the buffer.</returns>
+    public static implicit operator ReadOnlySpan<T>(BufferScope<T> scope)
+        => scope._span;
+}
