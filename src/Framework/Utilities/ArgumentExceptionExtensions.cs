241579d50f4eaa30d54b31836ff2fdfc079d6db5
Dustin Campbell <dustin@teamcampbell.org>
2026-02-19 12:45:11 -0800

    Add ArgumentException.ThrowIfNullOrEmpty for IEnumerable<T>
    Add an ArgumentException.ThrowNullAndEmpty(IEnumerable<T>?, string?) static extension method that throws if the argument is null or contains no elements. To support this method, an IsEmpty extension property and TryGetCount extension method targeting IEnumerable<T> have also been added.

--- src/Framework/Utilities/ArgumentExceptionExtensions.cs
+++ src/Framework/Utilities/ArgumentExceptionExtensions.cs
@@ -2,7 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using Microsoft.Build.Utilities;
+using Microsoft.CodeAnalysis;
 
 namespace Microsoft.Build;
 
@@ -29,5 +33,38 @@ internal static partial class ArgumentExceptionExtensions
                 ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
             }
         }
+
+        /// <summary>
+        ///  Throws an exception if <paramref name="argument"/> is <see langword="null"/> or empty.
+        /// </summary>
+        /// <param name="argument">
+        ///  The string argument to validate as non-null and non-empty.
+        /// </param>
+        /// <param name="paramName">
+        ///  The name of the parameter with which <paramref name="argument"/> corresponds.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///  <paramref name="argument"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///  <paramref name="argument"/> is empty.
+        /// </exception>
+        public static void ThrowIfNullOrEmpty<T>([NotNull] IEnumerable<T>? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            ArgumentNullException.ThrowIfNull(argument, paramName);
+
+            if (argument.IsEmpty)
+            {
+                ThrowEmpty(argument, paramName);
+            }
+
+            Contract.ThrowIfNull(argument);
+        }
     }
+
+#pragma warning disable IDE0051 // Private member is unused.
+
+    [DoesNotReturn]
+    private static void ThrowEmpty<T>(IEnumerable<T> argument, string? paramName)
+        => throw new ArgumentException(SR.Argument_ContainsNoElements, paramName);
 }
---
+++ src/Framework/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Build.Utilities;
+
+internal static class EnumerableExtensions
+{
+    extension<TSource>(IEnumerable<TSource> source)
+    {
+        public bool IsEmpty => source.TryGetCount(out int count)
+            ? count == 0
+            : !source.Any();
+    }
+
+    /// <summary>
+    ///  Attempts to determine the number of elements in a sequence without forcing an enumeration.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+    /// <param name="source">A sequence that contains elements to be counted.</param>
+    /// <param name="count">
+    ///  When this method returns, contains the count of <paramref name="source" /> if successful,
+    ///  or zero if the method failed to determine the count.</param>
+    /// <returns>
+    ///  <see langword="true" /> if the count of <paramref name="source"/> can be determined without enumeration;
+    ///  otherwise, <see langword="false" />.
+    /// </returns>
+    /// <remarks>
+    ///  The method performs a series of type tests, identifying common subtypes whose
+    ///  count can be determined without enumerating; this includes <see cref="ICollection{T}"/>,
+    ///  <see cref="ICollection"/> as well as internal types used in the LINQ implementation.
+    ///
+    ///  The method is typically a constant-time operation, but ultimately this depends on the complexity
+    ///  characteristics of the underlying collection implementation.
+    /// </remarks>
+    public static bool TryGetCount<TSource>(this IEnumerable<TSource> source, out int count)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+#if NET
+        // Note: TryGetNonEnumeratedCount doesn't test for IReadOnlyCollection<T>.
+        if (source is IReadOnlyCollection<TSource> collection)
+        {
+            count = collection.Count;
+            return true;
+        }
+
+        return Enumerable.TryGetNonEnumeratedCount(source, out count);
+#else
+        if (source is ICollection<TSource> collectionOfT)
+        {
+            count = collectionOfT.Count;
+            return true;
+        }
+
+        if (source is IReadOnlyCollection<TSource> readOnlyCollectionOfT)
+        {
+            count = readOnlyCollectionOfT.Count;
+            return true;
+        }
+
+        if (source is ICollection collection)
+        {
+            count = collection.Count;
+            return true;
+        }
+
+        count = 0;
+        return false;
+#endif
+    }
+}
