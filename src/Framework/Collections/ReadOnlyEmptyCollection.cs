0f26b3e312500819faf1929a8d54200f13071cb1
Dustin Campbell <dustin@teamcampbell.org>
2026-02-19 14:50:40 -0800

    Move ReadOnlyEmptyCollection to Microsoft.Build.Framework
    - Add "OM_NotSupportedReadOnlyCollection" string resource to Microsoft.Build.Framework\Resources\SR.resx
    - Copy ReadOnlyEmptyCollection.cs from shared source.
    - Clean up ReadOnlyEmptyCollection.cs
      - Enable nullable
      - Add IReadOnlyCollection<T> to list of interfaces
      - Remove unhelpful XML doc comments
      - Add simply empty enumerator
      - General clean up
    - Add InvalidOperationException.Throw(string?) extension method
    - Remove ReadOnlyEmptyCollection.cs from Microsoft.Build.csproj, Microsoft.Build.Tasks.csproj, Microsoft.Build.Utilities.csproj, and MSBuild.csproj.
    
    Note: ReadOnlyEmptyCollection.cs isn't deleted from shared source because it is still compiled into MSBuildTaskHost.

---
+++ src/Framework/Collections/ReadOnlyEmptyCollection.cs
@@ -0,0 +1,89 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Microsoft.Build.Collections;
+
+/// <summary>
+/// A read-only wrapper over an empty collection.
+/// </summary>
+/// <typeparam name="T">Type of element in the collection.</typeparam>
+internal sealed class ReadOnlyEmptyCollection<T> : IReadOnlyCollection<T>, ICollection<T>, ICollection
+{
+    public static ReadOnlyEmptyCollection<T> Instance => field ??= new();
+
+    private ReadOnlyEmptyCollection()
+    {
+    }
+
+    public int Count => 0;
+
+    public bool IsReadOnly => true;
+
+    bool ICollection.IsSynchronized => false;
+
+    object ICollection.SyncRoot => this;
+
+    public void Add(T item)
+        => InvalidOperationException.Throw(SR.OM_NotSupportedReadOnlyCollection);
+
+    public void Clear()
+        => InvalidOperationException.Throw(SR.OM_NotSupportedReadOnlyCollection);
+
+    public bool Contains(T item)
+        => false;
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+    }
+
+    public bool Remove(T item)
+    {
+        InvalidOperationException.Throw(SR.OM_NotSupportedReadOnlyCollection);
+        return false;
+    }
+
+    void ICollection.CopyTo(Array array, int index)
+    {
+    }
+
+    public IEnumerator<T> GetEnumerator()
+        => Enumerator.Instance;
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => Enumerator.Instance;
+
+    private sealed class Enumerator : IEnumerator<T>
+    {
+        public static readonly Enumerator Instance = new();
+
+        private Enumerator()
+        {
+        }
+
+        public T Current
+        {
+            get
+            {
+                InvalidOperationException.Throw();
+                return default;
+            }
+        }
+
+        object IEnumerator.Current => Current!;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+            => false;
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ src/Framework/Utilities/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Build;
+
+internal static class ExceptionExtensions
+{
+    extension(InvalidOperationException)
+    {
+        /// <inheritdoc cref="InvalidOperationException(string)"/>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Throw(string? message = null)
+            => throw new InvalidOperationException(message);
+    }
+}
