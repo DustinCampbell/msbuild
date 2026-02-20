dcf30f32ebf59c56df25ddf407cc22dba7681b85
Dustin Campbell <dustin@teamcampbell.org>
2026-02-20 14:21:59 -0800

    Add ReadOnlyDictionary.Create factory method
    Add factory method to create System.Collections.ObjectModel.ReadOnlyDictionary<TKey, TValue> instances. If the input dictionary is null or empty, ReadOnlyDictionary<TKey, TValue>.Empty is returned.

---
+++ src/Framework/Collections/ReadOnlyDictionary.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.Build.Collections;
+
+internal static class ReadOnlyDictionary
+{
+    public static ReadOnlyDictionary<TKey, TValue> Create<TKey, TValue>(IDictionary<TKey, TValue>? dictionary)
+        where TKey : notnull
+        => dictionary is { Count: > 0 }
+            ? new ReadOnlyDictionary<TKey, TValue>(dictionary)
+            : ReadOnlyDictionary<TKey, TValue>.Empty;
+
+    public static ReadOnlyDictionary<TKey, TValue> Empty<TKey, TValue>()
+        where TKey : notnull
+        => ReadOnlyDictionary<TKey, TValue>.Empty;
+}
