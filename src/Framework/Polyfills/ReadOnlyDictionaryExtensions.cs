0ed37451e7e26ccab5a86758f50c4b16f4149938
Dustin Campbell <dustin@teamcampbell.org>
2026-02-20 14:20:30 -0800

    Add ReadOnlyDictionary<TKey, TValue>.Empty polyfill extension property
    Add static extension property for ReadOnlyDictionary<TKey, TValue>.Empty on non-.NET.

---
+++ src/Framework/Polyfills/ReadOnlyDictionaryExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !NET
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Microsoft.Build;
+
+internal static class ReadOnlyDictionaryExtensions
+{
+    private static class Empty<TKey, TValue>
+        where TKey : notnull
+    {
+        public static readonly ReadOnlyDictionary<TKey, TValue> Instance = new(new Dictionary<TKey, TValue>());
+    }
+
+    extension<TKey, TValue>(ReadOnlyDictionary<TKey, TValue>)
+        where TKey : notnull
+    {
+        public static ReadOnlyDictionary<TKey, TValue> Empty => Empty<TKey, TValue>.Instance;
+    }
+}
+#endif
