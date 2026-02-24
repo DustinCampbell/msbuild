2d64491141e2a8ee6fdb9ddde25f64df5767c882
Dustin Campbell <dustin@teamcampbell.org>
2026-02-23 16:01:32 -0800

    Add RefArrayBuilder<T> and RefStack<T> helper collections

---
+++ src/Framework/ExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Build;
+
+/// <summary>
+///  Helper class for throwing exceptions.
+/// </summary>
+internal static class ExceptionExtensions
+{
+    extension(InvalidOperationException)
+    {
+        /// <inheritdoc cref="InvalidOperationException(string)"/>
+        [DoesNotReturn]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void Throw(string? message)
+            => throw new InvalidOperationException(message);
+    }
+}
