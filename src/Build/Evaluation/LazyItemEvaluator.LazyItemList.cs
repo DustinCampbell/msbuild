eb8b3fe673bb34da910039c4ef987de702062bae
Dustin Campbell <dustin@teamcampbell.org>
2026-02-25 16:25:20 -0800

    Move LazyItemList to a separate file

---
+++ src/Build/Evaluation/LazyItemEvaluator.LazyItemList.cs
@@ -0,0 +1,298 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+#if DEBUG
+using System.Diagnostics;
+#endif
+using System.Linq;
+using Microsoft.Build.Collections;
+using Microsoft.Build.Shared;
+
+#nullable disable
+
+namespace Microsoft.Build.Evaluation;
+
+internal partial class LazyItemEvaluator<P, I, M, D>
+    where P : class, IProperty, IEquatable<P>, IValued
+    where I : class, IItem<M>, IMetadataTable
+    where M : class, IMetadatum
+    where D : class, IItemDefinition<M>
+{
+    private sealed class LazyItemList
+    {
+        private readonly LazyItemList _previous;
+        private readonly LazyItemOperation _operation;
+
+        private GlobSet _cachedGlobsToIgnore;
+        private OrderedItemDataCollection _cachedItems;
+        private bool _isReferenced;
+#if DEBUG
+        private int _applyCalls;
+#endif
+
+        public LazyItemList(LazyItemList previous, LazyItemOperation operation)
+        {
+            _previous = previous;
+            _operation = operation;
+        }
+
+        /// <summary>
+        /// The operation held by this node, used by <see cref="ComputeItems"/> to
+        /// inspect the concrete operation type (e.g. RemoveOperation, UpdateOperation).
+        /// </summary>
+        public LazyItemOperation Operation => _operation;
+
+        public I[] GetMatchedItems(GlobSet globsToIgnore)
+        {
+            OrderedItemDataCollection.Builder items = GetItemData(globsToIgnore);
+            using var builder = new RefArrayBuilder<I>(initialCapacity: items.Count);
+
+            foreach (ItemData data in items)
+            {
+                if (data.ConditionResult)
+                {
+                    builder.Add(data.Item);
+                }
+            }
+
+            return builder.AsSpan().ToArray();
+        }
+
+        public OrderedItemDataCollection.Builder GetItemData(GlobSet globsToIgnore)
+        {
+            // Cache results only on the LazyItemOperations whose results are required by an external caller (via GetItems). This means:
+            //   - Callers of GetItems who have announced ahead of time that they would reference an operation (via MarkAsReferenced())
+            // This includes: item references (Include="@(foo)") and metadata conditions (Condition="@(foo->Count()) == 0")
+            // Without ahead of time notifications more computation is done than needed when the results of a future operation are requested
+            // The future operation is part of another item list referencing this one (making this operation part of the tail).
+            // The future operation will compute this list but since no ahead of time notifications have been made by callers, it won't cache the
+            // intermediary operations that would be requested by those callers.
+            //   - Callers of GetItems that cannot announce ahead of time. This includes item referencing conditions on
+            // Item Groups and Item Elements. However, those conditions are performed eagerly outside of the LazyItemEvaluator, so they will run before
+            // any item referencing operations from inside the LazyItemEvaluator. This
+            //
+            // If the head of this LazyItemList is uncached, then the tail may contain cached and un-cached nodes.
+            // In this case we have to compute the head plus the part of the tail up to the first cached operation.
+            //
+            // The cache is based on a couple of properties:
+            // - uses immutable lists for structural sharing between multiple cached nodes (multiple include operations won't have duplicated memory for the common items)
+            // - if an operation is cached for a certain set of globsToIgnore, then the entire operation tail can be reused. This is because (i) the structure of LazyItemLists
+            // does not mutate: one can add operations on top, but the base never changes, and (ii) the globsToIgnore passed to the tail is the concatenation between
+            // the globsToIgnore received as an arg, and the globsToIgnore produced by the head (if the head is a Remove operation)
+
+            OrderedItemDataCollection items;
+            if (TryGetFromCache(globsToIgnore, out items))
+            {
+                return items.ToBuilder();
+            }
+            else
+            {
+                // tell the cache that this operation's result is needed by an external caller
+                // this is required for callers that cannot tell the item list ahead of time that
+                // they would be using an operation
+                MarkAsReferenced();
+
+                return ComputeItems(this, globsToIgnore);
+            }
+        }
+
+        private bool TryGetFromCache(GlobSet globsToIgnore, out OrderedItemDataCollection items)
+        {
+            if (_cachedItems != null && ReferenceEquals(globsToIgnore, _cachedGlobsToIgnore))
+            {
+                items = _cachedItems;
+                return true;
+            }
+
+            items = null;
+            return false;
+        }
+
+        private void ApplyOperation(OrderedItemDataCollection.Builder listBuilder, GlobSet globsToIgnore)
+        {
+#if DEBUG
+            CheckInvariant();
+#endif
+
+            _operation.Apply(listBuilder, globsToIgnore);
+
+            // cache results if somebody is referencing this operation
+            if (_isReferenced)
+            {
+                _cachedGlobsToIgnore = globsToIgnore;
+                _cachedItems = listBuilder.ToImmutable();
+            }
+#if DEBUG
+            _applyCalls++;
+            CheckInvariant();
+#endif
+        }
+
+#if DEBUG
+        private void CheckInvariant()
+        {
+            if (_isReferenced)
+            {
+                // After Apply has been called, the cache should be populated.
+                Debug.Assert(_applyCalls == 0 || _cachedItems != null, "Referenced operation should cache its result after Apply");
+            }
+            else
+            {
+                // non referenced operations should not be cached
+                Debug.Assert(_cachedItems == null);
+            }
+        }
+#endif
+
+        /// <summary>
+        /// Applies uncached item operations (include, remove, update) in order. Since Remove effectively overwrites Include or Update,
+        /// Remove operations are preprocessed (adding to globsToIgnore) to create a longer list of globs we don't need to process
+        /// properly because we know they will be removed. Update operations are batched as much as possible, meaning rather
+        /// than being applied immediately, they are combined into a dictionary of UpdateOperations that need to be applied. This
+        /// is to optimize the case in which as series of UpdateOperations, each of which affects a single ItemSpec, are applied to all
+        /// items in the list, leading to a quadratic-time operation.
+        /// </summary>
+        private static OrderedItemDataCollection.Builder ComputeItems(LazyItemList lazyItemList, GlobSet globsToIgnore)
+        {
+            // Stack of operations up to the first one that's cached (exclusive)
+            Stack<LazyItemList> itemListStack = new Stack<LazyItemList>();
+
+            OrderedItemDataCollection.Builder items = null;
+
+            // Keep a separate stack of lists of globs to ignore that only gets modified for Remove operations
+            Stack<GlobSet> globsToIgnoreStack = null;
+
+            for (var currentList = lazyItemList; currentList != null; currentList = currentList._previous)
+            {
+                var globsToIgnoreFromFutureOperations = globsToIgnoreStack?.Peek() ?? globsToIgnore;
+
+                OrderedItemDataCollection itemsFromCache;
+                if (currentList.TryGetFromCache(globsToIgnoreFromFutureOperations, out itemsFromCache))
+                {
+                    // the base items on top of which to apply the uncached operations are the items of the first operation that is cached
+                    items = itemsFromCache.ToBuilder();
+                    break;
+                }
+
+                // If this is a remove operation, then add any globs that will be removed
+                //  to a list of globs to ignore in previous operations
+                if (currentList._operation is RemoveOperation removeOperation)
+                {
+                    globsToIgnoreStack ??= new Stack<GlobSet>();
+
+                    var globsToIgnoreForPreviousOperations = removeOperation.GetRemovedGlobs();
+                    foreach (var globToRemove in globsToIgnoreFromFutureOperations.Globs)
+                    {
+                        globsToIgnoreForPreviousOperations.Add(globToRemove);
+                    }
+
+                    globsToIgnoreStack.Push(GlobSet.Create(globsToIgnoreForPreviousOperations));
+                }
+
+                itemListStack.Push(currentList);
+            }
+
+            if (items == null)
+            {
+                items = OrderedItemDataCollection.CreateBuilder();
+            }
+
+            GlobSet currentGlobsToIgnore = globsToIgnoreStack == null ? globsToIgnore : globsToIgnoreStack.Peek();
+
+            Dictionary<string, UpdateOperation> itemsWithNoWildcards = new Dictionary<string, UpdateOperation>(StringComparer.OrdinalIgnoreCase);
+            bool addedToBatch = false;
+
+            // Walk back down the stack of item lists applying operations
+            while (itemListStack.Count > 0)
+            {
+                var currentList = itemListStack.Pop();
+
+                if (currentList._operation is UpdateOperation op)
+                {
+                    bool addToBatch = true;
+                    int i;
+                    // The TextFragments are things like abc.def or x*y.*z.
+                    for (i = 0; i < op.Spec.Fragments.Count; i++)
+                    {
+                        ItemSpecFragment frag = op.Spec.Fragments[i];
+                        if (MSBuildConstants.CharactersForExpansion.Any(frag.TextFragment.Contains))
+                        {
+                            // Fragment contains wild cards, items, or properties. Cannot batch over it using a dictionary.
+                            addToBatch = false;
+                            break;
+                        }
+
+                        string fullPath = FileUtilities.NormalizePathForComparisonNoThrow(frag.TextFragment, frag.ProjectDirectory);
+                        if (itemsWithNoWildcards.ContainsKey(fullPath))
+                        {
+                            // Another update will already happen on this path. Make that happen before evaluating this one.
+                            addToBatch = false;
+                            break;
+                        }
+                        else
+                        {
+                            itemsWithNoWildcards.Add(fullPath, op);
+                        }
+                    }
+                    if (!addToBatch)
+                    {
+                        // We found a wildcard. Remove any fragments associated with the current operation and process them later.
+                        for (int j = 0; j < i; j++)
+                        {
+                            itemsWithNoWildcards.Remove(currentList._operation.Spec.Fragments[j].TextFragment);
+                        }
+                    }
+                    else
+                    {
+                        addedToBatch = true;
+                        continue;
+                    }
+                }
+
+                if (addedToBatch)
+                {
+                    addedToBatch = false;
+                    ProcessNonWildCardItemUpdates(itemsWithNoWildcards, items);
+                }
+
+                // If this is a remove operation, then it could modify the globs to ignore, so pop the potentially
+                //  modified entry off the stack of globs to ignore
+                if (currentList._operation is RemoveOperation)
+                {
+                    globsToIgnoreStack.Pop();
+                    currentGlobsToIgnore = globsToIgnoreStack.Count == 0 ? globsToIgnore : globsToIgnoreStack.Peek();
+                }
+
+                currentList.ApplyOperation(items, currentGlobsToIgnore);
+            }
+
+            // We finished looping through the operations. Now process the final batch if necessary.
+            ProcessNonWildCardItemUpdates(itemsWithNoWildcards, items);
+
+            return items;
+        }
+
+        private static void ProcessNonWildCardItemUpdates(Dictionary<string, UpdateOperation> itemsWithNoWildcards, OrderedItemDataCollection.Builder items)
+        {
+            if (itemsWithNoWildcards.Count > 0)
+            {
+                for (int i = 0; i < items.Count; i++)
+                {
+                    string fullPath = FileUtilities.NormalizePathForComparisonNoThrow(items[i].Item.EvaluatedInclude, items[i].Item.ProjectDirectory);
+                    if (itemsWithNoWildcards.TryGetValue(fullPath, out UpdateOperation op))
+                    {
+                        items[i] = op.UpdateItem(items[i]);
+                    }
+                }
+                itemsWithNoWildcards.Clear();
+            }
+        }
+
+        public void MarkAsReferenced()
+        {
+            _isReferenced = true;
+        }
+    }
+}
