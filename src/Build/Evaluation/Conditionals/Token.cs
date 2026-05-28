8c40c6f2a58f7ea5371bcb03ce26645b692373ab
Dustin Campbell <dustin@teamcampbell.org>
2026-05-28 14:40:07 -0700

    Convert Token from class to readonly struct
    Eliminates allocations for tokens produced during condition parsing. Static readonly fields provide shared instances for operator/punctuation tokens, and factory methods create value-type instances for tokens with text content.

--- src/Build/Evaluation/Conditionals/Token.cs
+++ src/Build/Evaluation/Conditionals/Token.cs
@@ -1,146 +1,125 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-
 #nullable disable
 
 namespace Microsoft.Build.Evaluation
 {
     /// <summary>
-    /// This class represents a token in the Complex Conditionals grammar.  It's
-    /// really just a bag that contains the type of the token and the string that
-    /// was parsed into the token.  This isn't very useful for operators, but
-    /// is useful for strings and such.
+    /// Valid token kinds in the conditional expression grammar.
     /// </summary>
-    internal sealed class Token
+    internal enum TokenKind
     {
-        internal static readonly Token Comma = new Token(TokenType.Comma);
-        internal static readonly Token LeftParenthesis = new Token(TokenType.LeftParenthesis);
-        internal static readonly Token RightParenthesis = new Token(TokenType.RightParenthesis);
-        internal static readonly Token LessThan = new Token(TokenType.LessThan);
-        internal static readonly Token GreaterThan = new Token(TokenType.GreaterThan);
-        internal static readonly Token LessThanOrEqualTo = new Token(TokenType.LessThanOrEqualTo);
-        internal static readonly Token GreaterThanOrEqualTo = new Token(TokenType.GreaterThanOrEqualTo);
-        internal static readonly Token And = new Token(TokenType.And);
-        internal static readonly Token Or = new Token(TokenType.Or);
-        internal static readonly Token EqualTo = new Token(TokenType.EqualTo);
-        internal static readonly Token NotEqualTo = new Token(TokenType.NotEqualTo);
-        internal static readonly Token Not = new Token(TokenType.Not);
-        internal static readonly Token EndOfInput = new Token(TokenType.EndOfInput);
+        Comma,
+        LeftParenthesis,
+        RightParenthesis,
+
+        LessThan,
+        GreaterThan,
+        LessThanOrEqualTo,
+        GreaterThanOrEqualTo,
+
+        And,
+        Or,
+
+        EqualTo,
+        NotEqualTo,
+        Not,
+
+        Property,
+        String,
+        Number,
+        ItemList,
+        ItemMetadata,
+        FunctionName,
+
+        EndOfInput,
+    }
+
+    /// <summary>
+    /// Represents a token in the conditional expression grammar.
+    /// </summary>
+    internal readonly struct Token
+    {
+        public static readonly Token Comma = new(TokenKind.Comma);
+        public static readonly Token LeftParenthesis = new(TokenKind.LeftParenthesis);
+        public static readonly Token RightParenthesis = new(TokenKind.RightParenthesis);
+        public static readonly Token LessThan = new(TokenKind.LessThan);
+        public static readonly Token GreaterThan = new(TokenKind.GreaterThan);
+        public static readonly Token LessThanOrEqualTo = new(TokenKind.LessThanOrEqualTo);
+        public static readonly Token GreaterThanOrEqualTo = new(TokenKind.GreaterThanOrEqualTo);
+        public static readonly Token And = new(TokenKind.And);
+        public static readonly Token Or = new(TokenKind.Or);
+        public static readonly Token EqualTo = new(TokenKind.EqualTo);
+        public static readonly Token NotEqualTo = new(TokenKind.NotEqualTo);
+        public static readonly Token Not = new(TokenKind.Not);
+        public static readonly Token EndOfInput = new(TokenKind.EndOfInput);
+
+        private readonly string _text;
+
+        public TokenKind Kind { get; }
 
         /// <summary>
-        /// Valid tokens
+        ///  Whether the content potentially has expandable content,
+        ///  such as a property expression or escaped character.
         /// </summary>
-        internal enum TokenType
+        public bool Expandable { get; }
+
+        private Token(TokenKind kind)
         {
-            Comma,
-            LeftParenthesis,
-            RightParenthesis,
-
-            LessThan,
-            GreaterThan,
-            LessThanOrEqualTo,
-            GreaterThanOrEqualTo,
-
-            And,
-            Or,
-
-            EqualTo,
-            NotEqualTo,
-            Not,
-
-            Property,
-            String,
-            Numeric,
-            ItemList,
-            ItemMetadata,
-            Function,
-
-            EndOfInput,
+            Kind = kind;
+            _text = null;
         }
 
-        private TokenType _tokenType;
-        private string _tokenString;
-
-        /// <summary>
-        /// Constructor for types that don't have values
-        /// </summary>
-        /// <param name="tokenType"></param>
-        private Token(TokenType tokenType)
+        private Token(TokenKind kind, string text, bool expandable = false)
         {
-            _tokenType = tokenType;
-            _tokenString = null;
+            Assumed.NotNull(text);
+
+            Kind = kind;
+            _text = text;
+            Expandable = expandable;
         }
 
-        /// <summary>
-        /// Constructor takes the token type and the string that
-        /// represents the token
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="tokenString"></param>
-        internal Token(TokenType type, string tokenString)
-            : this(type, tokenString, false /* not expandable */)
-        { }
+        public static Token Number(string text)
+            => new(TokenKind.Number, text);
 
-        /// <summary>
-        /// Constructor takes the token type and the string that
-        /// represents the token.
-        /// If the string may contain content that needs expansion, expandable is set.
-        /// </summary>
-        internal Token(TokenType type, string tokenString, bool expandable)
-        {
-            Assumed.True(
-                type is TokenType.Property or
-                        TokenType.String or
-                        TokenType.Numeric or
-                        TokenType.ItemList or
-                        TokenType.ItemMetadata or
-                        TokenType.Function,
-                "Unexpected token type");
+        public static Token FunctionName(string text)
+            => new(TokenKind.FunctionName, text);
 
-            Assumed.NotNull(tokenString);
+        public static Token Property(string text)
+            => new(TokenKind.Property, text);
 
-            _tokenType = type;
-            _tokenString = tokenString;
-            this.Expandable = expandable;
-        }
+        public static Token ItemMetadata(string text)
+            => new(TokenKind.ItemMetadata, text);
 
-        /// <summary>
-        /// Whether the content potentially has expandable content,
-        /// such as a property expression or escaped character.
-        /// </summary>
-        internal bool Expandable
-        {
-            get;
-            set;
-        }
+        public static Token ItemList(string text)
+            => new(TokenKind.ItemList, text);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        internal bool IsToken(TokenType type)
-        {
-            return _tokenType == type;
-        }
+        public static Token String(string text, bool expandable = false)
+            => new(TokenKind.String, text, expandable);
 
-        internal string String
-            => _tokenString ?? _tokenType switch
+        public bool IsKind(TokenKind kind)
+            => Kind == kind;
+
+        public override string ToString()
+            => Text;
+
+        internal string Text
+            => _text ?? Kind switch
             {
-                TokenType.Comma => ",",
-                TokenType.LeftParenthesis => "(",
-                TokenType.RightParenthesis => ")",
-                TokenType.LessThan => "<",
-                TokenType.GreaterThan => ">",
-                TokenType.LessThanOrEqualTo => "<=",
-                TokenType.GreaterThanOrEqualTo => ">=",
-                TokenType.And => "and",
-                TokenType.Or => "or",
-                TokenType.EqualTo => "==",
-                TokenType.NotEqualTo => "!=",
-                TokenType.Not => "!",
-                TokenType.EndOfInput => null,
+                TokenKind.Comma => ",",
+                TokenKind.LeftParenthesis => "(",
+                TokenKind.RightParenthesis => ")",
+                TokenKind.LessThan => "<",
+                TokenKind.GreaterThan => ">",
+                TokenKind.LessThanOrEqualTo => "<=",
+                TokenKind.GreaterThanOrEqualTo => ">=",
+                TokenKind.And => "and",
+                TokenKind.Or => "or",
+                TokenKind.EqualTo => "==",
+                TokenKind.NotEqualTo => "!=",
+                TokenKind.Not => "!",
+                TokenKind.EndOfInput => null,
                 _ => Assumed.Unreachable<string>(),
             };
     }
